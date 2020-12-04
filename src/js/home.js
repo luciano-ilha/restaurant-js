273d14284dc6fb6c5cc1ce1759c4f622e5c440ed
Luciano Ilha Carbonell <ilha.dev.br@gmail.com>
2020-12-04 08:01:13 -0300

    Delete unnecessary img and change home js file

--- src/js/home.js
+++ src/js/home.js
@@ -1,5 +1,9 @@
 import img from '../assets/lph_logo.png'
 const mainContent = document.getElementById('content');
+const menuEl = document.createElement('div');
+const elHome = document.createElement('span');
+const elMenu = document.createElement('span');
+const elContact = document.createElement('span');
 
 const content = () => {
   mainContent.classList.add('d-flex');
@@ -9,23 +13,17 @@ const navigation = () => {
   const navEl = document.createElement('div');
   navEl.classList.add('nav', 'd-flex');
   mainContent.appendChild(navEl);
-  
   const imgEl = new Image();
   imgEl.src = img;
   navEl.appendChild(imgEl);
-  
-  const menuEl = document.createElement('div');
   menuEl.classList.add('nav-menu', 'd-flex');
   navEl.appendChild(menuEl);
-  const elHome = document.createElement('span');
   elHome.classList.add('nav-menu-item');
   elHome.innerHTML = "HOME";
   menuEl.appendChild(elHome);
-  const elMenu = document.createElement('span');
   elMenu.classList.add('nav-menu-item');
   elMenu.innerHTML = "MENU";
   menuEl.appendChild(elMenu)
-  const elContact = document.createElement('span');
   elContact.classList.add('nav-menu-item');
   elContact.innerHTML = "CONTACT";
   menuEl.appendChild(elContact)
